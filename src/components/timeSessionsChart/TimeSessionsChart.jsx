ccdef66751f22edd06a7b54b3a05a6fd851e33e8
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-04-27 16:41:12 +0200

    time session chart update

--- src/components/timeSessionsChart/TimeSessionsChart.jsx
+++ src/components/timeSessionsChart/TimeSessionsChart.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import FetchData from '../customHook/FetchData';
 import { UserSessionsFactory } from '../../factories/UserSessionsFactory';
+import variables from '../../styles/_settings.scss';
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Text,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 /**
  * React component given the structure HTML of the chart with sessions informations
@@ -10,6 +22,7 @@ import { UserSessionsFactory } from '../../factories/UserSessionsFactory';
  */
 const TimeSessionsChart = ({ userId }) => {
   const url = '/mockedDatas/sessionsDuration.json';
+  const [dataArray, setDataArray] = useState([]);
 
   const [data, isLoading, isError, error] = FetchData(
     url,
@@ -17,6 +30,43 @@ const TimeSessionsChart = ({ userId }) => {
     UserSessionsFactory,
     'api'
   );
+
+  /**
+   * recharts tooltip which render the active value of the chart by html nodes
+   * https://recharts.org/en-US/api/Tooltip
+   * @param {Boolean} active If set true, the tooltip is displayed. If set false, the tooltip is hidden
+   * @param {object} payload array of the content to be displayed in the tooltip
+   * @param {string} label label value which is active
+   */
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p>{payload[0].value} min</p>
+        </div>
+      );
+    }
+  };
+
+  /**
+   * customize the legend of the graph
+   * @returns customized legend components
+   */
+  function CustomLegend() {
+    return (
+      <div className="graph-legend">
+        <p>Durée moyenne des</p>
+        <p>sessions</p>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    if (!isNaN(userId) && data) {
+      setDataArray(data.sessions);
+      console.log(dataArray);
+    }
+  }, [userId, data]);
 
   if (isLoading) return <p>Données en cours de chargement</p>;
 
@@ -30,15 +80,35 @@ const TimeSessionsChart = ({ userId }) => {
 
   return (
     <div className="timeSessionsChart-container">
-      <p>Sessions infos Chart</p>
-      <p>{userId}</p>
-      <ul>
-        {data.sessions.map((session, index) => (
-          <li key={index}>
-            day: {session.day} duration: {session.sessionLength} min
-          </li>
-        ))}
-      </ul>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={dataArray}
+          margin={{ top: 5, right: 10, left: 10, bottom: 5 }}
+        >
+          <Legend content={<CustomLegend />} align="left" verticalAlign="top" />
+          <Line
+            type="natural"
+            dataKey="sessionLength"
+            stroke={variables.white1}
+            strokeWidth={2}
+            dot={false}
+          />
+          {/* <CartesianGrid stroke={variables.red2} /> */}
+          <XAxis
+            dataKey="day"
+            stroke="rgba(255,255,255,0.5)"
+            tickLine={false}
+            axisLine={false}
+            // padding={{ left: 5 }}
+            
+          />
+          {/* <YAxis /> */}
+          <Tooltip
+            content={<CustomTooltip />}
+            wrapperStyle={{ outline: 'none' }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
--- src/models/api/UserSessions.js
+++ src/models/api/UserSessions.js
@@ -10,9 +10,28 @@ export class UserSessions {
     return this._id;
   }
   get sessions() {
-    let arrayOfSessions = this._sessions.map(
-      (session) => new SessionDuration(session)
-    );
+    let arrayOfSessions = this._sessions.map((session) => {
+      const data = new SessionDuration(session);
+      switch (data.day) {
+        case 1:
+          return { day: 'L', sessionLength: data.sessionLength };
+        case 2:
+          return { day: 'M', sessionLength: data.sessionLength };
+        case 3:
+          return { day: 'M', sessionLength: data.sessionLength };
+        case 4:
+          return { day: 'J', sessionLength: data.sessionLength };
+        case 5:
+          return { day: 'V', sessionLength: data.sessionLength };
+        case 6:
+          return { day: 'S', sessionLength: data.sessionLength };
+        case 7:
+          return { day: 'D', sessionLength: data.sessionLength };
+        default:
+          return { day: data.day, sessionLength: data.sessionLength };
+      }
+    });
+    console.log(arrayOfSessions);
     return arrayOfSessions;
   }
 }
